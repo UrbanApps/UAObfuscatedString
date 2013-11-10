a69624e3718d3d169079275a8fe50bc962568fd6
Matt Coneybeare <coneybeare@gmail.com>
2013-11-10 15:00:06 -0500

    Add UAObfuscatedString 0.1

---
+++ NSString+UAObfuscatedString.m
@@ -0,0 +1,80 @@
+//
+//  NSString+UAObfuscatedString.m
+//
+//  Created by Matt Coneybeare on 11/10/13.
+//  Copyright (c) 2013 Urban Apps. All rights reserved.
+//
+
+#import "NSString+UAObfuscatedString.h"
+
+@implementation NSString (UAObfuscatedString)
+
+- (NSString *)a { return [self stringByAppendingString:@"a"]; }
+- (NSString *)b { return [self stringByAppendingString:@"b"]; }
+- (NSString *)c { return [self stringByAppendingString:@"c"]; }
+- (NSString *)d { return [self stringByAppendingString:@"d"]; }
+- (NSString *)e { return [self stringByAppendingString:@"e"]; }
+- (NSString *)f { return [self stringByAppendingString:@"f"]; }
+- (NSString *)g { return [self stringByAppendingString:@"g"]; }
+- (NSString *)h { return [self stringByAppendingString:@"h"]; }
+- (NSString *)i { return [self stringByAppendingString:@"i"]; }
+- (NSString *)j { return [self stringByAppendingString:@"j"]; }
+- (NSString *)k { return [self stringByAppendingString:@"k"]; }
+- (NSString *)l { return [self stringByAppendingString:@"l"]; }
+- (NSString *)m { return [self stringByAppendingString:@"m"]; }
+- (NSString *)n { return [self stringByAppendingString:@"n"]; }
+- (NSString *)o { return [self stringByAppendingString:@"o"]; }
+- (NSString *)p { return [self stringByAppendingString:@"p"]; }
+- (NSString *)q { return [self stringByAppendingString:@"q"]; }
+- (NSString *)r { return [self stringByAppendingString:@"r"]; }
+- (NSString *)s { return [self stringByAppendingString:@"s"]; }
+- (NSString *)t { return [self stringByAppendingString:@"t"]; }
+- (NSString *)u { return [self stringByAppendingString:@"u"]; }
+- (NSString *)v { return [self stringByAppendingString:@"v"]; }
+- (NSString *)w { return [self stringByAppendingString:@"w"]; }
+- (NSString *)x { return [self stringByAppendingString:@"x"]; }
+- (NSString *)y { return [self stringByAppendingString:@"y"]; }
+- (NSString *)z { return [self stringByAppendingString:@"z"]; }
+
+- (NSString *)A { return [self stringByAppendingString:@"A"]; }
+- (NSString *)B { return [self stringByAppendingString:@"B"]; }
+- (NSString *)C { return [self stringByAppendingString:@"C"]; }
+- (NSString *)D { return [self stringByAppendingString:@"D"]; }
+- (NSString *)E { return [self stringByAppendingString:@"E"]; }
+- (NSString *)F { return [self stringByAppendingString:@"F"]; }
+- (NSString *)G { return [self stringByAppendingString:@"G"]; }
+- (NSString *)H { return [self stringByAppendingString:@"H"]; }
+- (NSString *)I { return [self stringByAppendingString:@"I"]; }
+- (NSString *)J { return [self stringByAppendingString:@"J"]; }
+- (NSString *)K { return [self stringByAppendingString:@"K"]; }
+- (NSString *)L { return [self stringByAppendingString:@"L"]; }
+- (NSString *)M { return [self stringByAppendingString:@"M"]; }
+- (NSString *)N { return [self stringByAppendingString:@"N"]; }
+- (NSString *)O { return [self stringByAppendingString:@"O"]; }
+- (NSString *)P { return [self stringByAppendingString:@"P"]; }
+- (NSString *)Q { return [self stringByAppendingString:@"Q"]; }
+- (NSString *)R { return [self stringByAppendingString:@"R"]; }
+- (NSString *)S { return [self stringByAppendingString:@"S"]; }
+- (NSString *)T { return [self stringByAppendingString:@"T"]; }
+- (NSString *)U { return [self stringByAppendingString:@"U"]; }
+- (NSString *)V { return [self stringByAppendingString:@"V"]; }
+- (NSString *)W { return [self stringByAppendingString:@"W"]; }
+- (NSString *)X { return [self stringByAppendingString:@"X"]; }
+- (NSString *)Y { return [self stringByAppendingString:@"Y"]; }
+- (NSString *)Z { return [self stringByAppendingString:@"Z"]; }
+
+- (NSString *)_1 { return [self stringByAppendingString:@"1"]; }
+- (NSString *)_2 { return [self stringByAppendingString:@"2"]; }
+- (NSString *)_3 { return [self stringByAppendingString:@"3"]; }
+- (NSString *)_4 { return [self stringByAppendingString:@"4"]; }
+- (NSString *)_5 { return [self stringByAppendingString:@"5"]; }
+- (NSString *)_6 { return [self stringByAppendingString:@"6"]; }
+- (NSString *)_7 { return [self stringByAppendingString:@"7"]; }
+- (NSString *)_8 { return [self stringByAppendingString:@"8"]; }
+- (NSString *)_9 { return [self stringByAppendingString:@"9"]; }
+- (NSString *)_0 { return [self stringByAppendingString:@"0"]; }
+
+- (NSString *)space { return [self stringByAppendingString:@" "]; }
+- (NSString *)dot { return [self stringByAppendingString:@"."]; }
+
+@end
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-UAObfuscatedString
-==================
+## UAObfuscatedString
 
-A simple NSStringCategory to hide sensitive strings from appearing in your binary
+UAObfuscatedString is a simple and lightweight category on NSString that allows you to prevent sensitive strings from appearing in the binary. Without some sort of obfuscation, strings like backend API methods and urls, API keys and other sensitive data can be read by utilizing various tools such as `strings`.
+
+## Installation
+
+1. Add `NSString+UAObfuscatedString.[h|m]` to your project. 
+2. Add `import "NSString+UAObfuscatedString.h"` where you want to use it.
+
+## Usage
+
+The category is very simple to use. Each letter a-Z has been changed into a method name which appends the letter to the caller. numbers are prefixed with an underscore, and there are two special methods for space and period.
+
+    NSString *root = @"";
+    NSLog(@"%@", root._1.dot.space._2.dot.space._3.dot.space.T.h.i.s.space.i.s.space.a.space.t.e.s.t.dot); 
+  
+    > 1. 2. 3. This is a test.
+ 
+
+Usually, you will use this on things like your in-app purchase identifiers
+
+    NSString *identifier = @"".c.o.m.dot.u.r.b.a.n.a.p.p.s.dot.e.x.a.m.p.l.e;
+    
+There are many ways to obfuscate strings, this is just one of them. Enjoy.
